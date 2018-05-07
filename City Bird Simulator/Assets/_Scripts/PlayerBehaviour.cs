5ae20b23c6cc270afee933aab61d70b1bbbdcb09
andrew-nessel <andrew.c.nessel@gmail.com>
2018-05-07 12:21:15 -0400

    Made some adjustments to the demo

--- City Bird Simulator/Assets/_Scripts/PlayerBehaviour.cs
+++ City Bird Simulator/Assets/_Scripts/PlayerBehaviour.cs
@@ -6,18 +6,21 @@ public class PlayerBehaviour : MonoBehaviour {
 
     public float speed;
     public float tilt;
+    public float Rtilt;
 
     public Rigidbody rb;
+    private Vector3 previousVelocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tilt = 0.5f;
+        Rtilt = 0.0f;
         speed = 0.0f;
     }
 	
 	void FixedUpdate () {
-
+        
 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -27,29 +30,39 @@ public class PlayerBehaviour : MonoBehaviour {
             tilt = 1f;
         if (tilt < 0)
             tilt = 0f;
-        
+
+        Rtilt = Rtilt - horizontal;
+        if (Rtilt > 1)
+            Rtilt = 1f;
+        if (Rtilt < -1)
+            Rtilt = -1f;
+
         float lift = tilt * 6f + 6.8f;
         float fallSpeed = 9.8f - lift;
 
         float momentum = fallSpeed * .1f;
 
-        //if (fallSpeed < 0.0f)
-        //{
-        //    momentum = 0.0f - momentum;
-        //}
-
         speed = speed + momentum;
 
-        if (speed < -0.01f)
+        if (speed < 0.01f)
         {
             speed = 0.0f;
             lift = 9.8f;
+            rb.velocity = new Vector3 (0, 0, 0);
+        }else if(speed > 30.0f)
+        {
+            speed = 10.0f;
         }
 
-        Debug.Log(speed);
+        Debug.Log((speed) + " " + (Rtilt * speed));
+        
 
-        Vector3 liftVector = new Vector3(0.0f, lift, 0.0f);
-        rb.AddForce(liftVector);       
+        Vector3 liftVector = new Vector3(Rtilt * speed, lift, speed);
+        rb.AddRelativeForce(liftVector); 
+
+        //rb.AddRelativeForce(Vector3.forward * speed);
+
+        previousVelocity = rb.velocity;
 
 	}
 
