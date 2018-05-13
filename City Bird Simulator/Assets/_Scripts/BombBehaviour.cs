647a2030a5f4b630d0f4cda5315fbbb7f306ac4d
mzhong1 <mzhong.phiria@yahoo.com>
2018-05-13 00:51:41 -0400

    Changes
    Okay, I think things are fixed...

--- City Bird Simulator/Assets/_Scripts/BombBehaviour.cs
+++ City Bird Simulator/Assets/_Scripts/BombBehaviour.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BombBehaviour : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public float speed = 420.0f;
+    public int predictionStepsPerFrame = 6;
+    public Vector3 bombVelocity;
+    // Use this for initialization
+    void Start () {
+        bombVelocity = this.transform.forward * speed;
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.useGravity = true;
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        Vector3 point1 = this.transform.position;
+        float stepSize = 1.0f / predictionStepsPerFrame;
+        for (float step = 0; step < 1; step += stepSize)
+        {
+            bombVelocity += Physics.gravity * stepSize * Time.deltaTime;
+            Vector3 point2 = point1 + bombVelocity * stepSize * Time.deltaTime;
 
-        if(collision.gameObject.tag == "Target")
+            Ray ray = new Ray(point1, point2 - point1);
+            if (Physics.Raycast(ray, (point2 - point1).magnitude))
+            {
+                //Some poor target has been hit
+                Debug.Log("Hit");
+            }
+            point1 = point2;
+        }
+        this.transform.position = point1;
+
+        if (collision.gameObject.tag == "Target")
         {
             Debug.Log("Bomb hit a TARGET");
             Destroy(gameObject);
@@ -28,6 +47,21 @@ public class BombBehaviour : MonoBehaviour {
             Destroy(gameObject);
         }
 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        float stepSize = 0.01f;
+        Vector3 point1 = this.transform.position;
+        Vector3 predictedBombVelocity = bombVelocity;
+        for (float step = 0; step < 1; step += stepSize)
+        {
+            predictedBombVelocity += Physics.gravity * stepSize;
+            Vector3 point2 = point1 + bombVelocity * stepSize;
+            Gizmos.DrawLine(point1, point2);
+            point1 = point2;
+        }
     }
 
 }
