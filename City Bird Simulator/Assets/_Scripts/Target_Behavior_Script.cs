ff5ccc899d24b14046006d3bf7ae4871e647b7c2
abangs730 <39225677+abangs730@users.noreply.github.com>
2018-05-27 15:49:55 -0400

    Target colors, thermal particles, level size
    added a small script to the targets to change theri color, increased size a bit, changed thermals so particles can be modified

---
+++ City Bird Simulator/Assets/_Scripts/Target_Behavior_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Behavior_Script : MonoBehaviour {
+	
+	Material m_Material;
+	
+	void Start(){
+		m_Material = GetComponent<Renderer>().material;
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag("Bomb")){
+			this.m_Material.color = Color.white;
+		}
+	}
+}
