92d25b93e1cbe7f23d0c68631ead120b35f22366
andrew-nessel <andrew.c.nessel@gmail.com>
2018-06-03 16:39:12 -0400

    Revert "Target Sounds Added"
    This reverts commit b2eff556165897bc13de3169e2ce6be1a089a4ef.

--- City Bird Simulator/Assets/_Scripts/Target_Behavior_Script.cs
+++ City Bird Simulator/Assets/_Scripts/Target_Behavior_Script.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Target_Behavior_Script : MonoBehaviour {
 	
 	Material m_Material;
-
-    public AudioClip TargetReaction;
-
-    private AudioSource sound;
+	
 	void Start(){
 		m_Material = GetComponent<Renderer>().material;
 	}
@@ -16,7 +13,6 @@ public class Target_Behavior_Script : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.CompareTag("Bomb")){
 			this.m_Material.color = Color.white;
-            sound.PlayOneShot(TargetReaction);
 		}
 	}
 }
