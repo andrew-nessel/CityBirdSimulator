9229e82b327319ac2e83b32fa3295ab73050922d
mzhong1 <mzhong.phiria@yahoo.com>
2018-06-03 21:38:16 -0400

    BGM
    Added BGM persistence

--- City Bird Simulator/Assets/_Scripts/Target_Behavior_Script.cs
+++ City Bird Simulator/Assets/_Scripts/Target_Behavior_Script.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class Target_Behavior_Script : MonoBehaviour {
 	
 	Material m_Material;
-	
-	void Start(){
+    public AudioClip Damn;
+    public AudioClip GodDammit;
+    public AudioClip No;
+    public AudioClip Sigh;
+    public AudioClip Great;
+    public AudioClip Why;
+    public AudioClip Yo;
+    public AudioClip Kidding;
+    private AudioSource sound;
+    void Start(){
 		m_Material = GetComponent<Renderer>().material;
 	}
 	
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.CompareTag("Bomb")){
 			this.m_Material.color = Color.white;
-		}
+            int rand = Random.Range(0, 8);
+            if (rand < 1f)
+            {
+                sound.PlayOneShot(Damn);
+            }
+            else if (rand < 2f)
+            {
+                sound.PlayOneShot(GodDammit);
+            }
+            else if (rand < 3f)
+            {
+                sound.PlayOneShot(No);
+            }
+            else if (rand < 4f)
+            {
+                sound.PlayOneShot(Sigh);
+            }
+            else if (rand < 5f)
+            {
+                sound.PlayOneShot(Great);
+            }
+            else if (rand < 6f)
+            {
+                sound.PlayOneShot(Why);
+            }
+            else if (rand < 7f)
+            {
+                sound.PlayOneShot(Yo);
+            }
+            else if (rand < 8f)
+            {
+                sound.PlayOneShot(Kidding);
+            }
+
+        }
 	}
 }
