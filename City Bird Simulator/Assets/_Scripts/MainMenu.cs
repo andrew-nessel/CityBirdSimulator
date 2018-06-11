69b35eff21139c94e40960241814a8a7372d82a9
andrew-nessel <andrew.c.nessel@gmail.com>
2018-06-11 00:43:54 -0400

    Changed title screen, added credits, and how to play screens

--- City Bird Simulator/Assets/_Scripts/MainMenu.cs
+++ City Bird Simulator/Assets/_Scripts/MainMenu.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement; // neded in order to load scenes
 public class MainMenu : MonoBehaviour
 {
 
+    public GameObject CreditsUI;
+    public GameObject MainMenuUI;
+    public GameObject InstructionsUI;
+    public GameObject ObjectivesUI;
+
     // Use this for initialization
     void Start()
     {
-
+        CreditsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+        InstructionsUI.SetActive(false);
+        ObjectivesUI.SetActive(false);
     }
     public void StartLevel() //this function will be used on our Play button
 
@@ -22,6 +30,38 @@ public class MainMenu : MonoBehaviour
     {
 
     }
+
+    public void BacktoMain()
+    {
+        CreditsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+        InstructionsUI.SetActive(false);
+        ObjectivesUI.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        CreditsUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+        InstructionsUI.SetActive(false);
+        ObjectivesUI.SetActive(false);
+    }
+
+    public void ShowHowTo1()
+    {
+        CreditsUI.SetActive(false);
+        MainMenuUI.SetActive(false);
+        InstructionsUI.SetActive(true);
+        ObjectivesUI.SetActive(false);
+    }
+    public void ShowHowTo2()
+    {
+        CreditsUI.SetActive(false);
+        MainMenuUI.SetActive(false);
+        InstructionsUI.SetActive(false);
+        ObjectivesUI.SetActive(true);
+    }
+
     public void ExitGame() //This function will be used on our "Yes" button in our Quit menu
 
     {
